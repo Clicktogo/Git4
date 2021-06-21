bf476315cc34a9be8f6d51d6863f12a5a6aabd95
Filip Garcia <mailafilip@gmail.com>
2021-06-21 10:29:22 +0200

    test

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("git");
+
+        List<Integer> list = new ArrayList<>();
+        list = IntStream.range(1, 10).boxed().collect(Collectors.toList());
+        System.out.println(list);
+
+        System.out.println(findLowestValue(list));
+
+    }
+
+    public static Integer findLowestValue(List<Integer> list){
+       return list.stream().min(Comparator.naturalOrder()).get();
     }
 }
