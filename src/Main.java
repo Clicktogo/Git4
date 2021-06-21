bdb111a0f36376b976535a72d899bd074777619a
Filip Garcia <mailafilip@gmail.com>
2021-06-21 11:00:54 +0200

    test2

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,6 @@ public class Main {
 
         System.out.println(findLowestValue(list));
         System.out.println(findMaxValue(list));
-
     }
 
     public static Integer findLowestValue(List<Integer> list){
