1341c232b3dd0b20ff241811a4a67fcaad559e21
Filip Garcia <mailafilip@gmail.com>
2021-06-21 10:41:38 +0200

    No print

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,9 @@ import java.util.stream.Stream;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("git");
 
         List<Integer> list = new ArrayList<>();
         list = IntStream.range(1, 20).boxed().collect(Collectors.toList());
-        System.out.println(list);
 
         System.out.println(findLowestValue(list));
 
